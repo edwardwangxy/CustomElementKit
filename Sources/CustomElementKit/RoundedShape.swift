f4343b6eb32264493ae329d59840aa51cdb0f333
Edward Wang <admin@uwwxy.com>
2020-09-23 11:49:13 -0400

    add rounded corner

---
+++ Sources/CustomElementKit/RoundedShape.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by Xiangyu Wang on 9/23/20.
+//
+
+import Foundation
+import SwiftUI
+
+struct RoundedCorner: Shape {
+
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+
