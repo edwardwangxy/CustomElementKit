e023439c89eeebec911efab000bfed520e867d13
Edward Wang <edwardwangxy@gmail.com>
2021-02-18 20:14:35 -0500

    public function

--- Sources/CustomElementKit/NavigationBarConfigurator.swift
+++ Sources/CustomElementKit/NavigationBarConfigurator.swift
@@ -7,13 +7,17 @@
 import SwiftUI
 
 
-struct NavigationBarConfigurator: UIViewControllerRepresentable {
-    var configure: (UINavigationController) -> Void = { _ in }
-
-    func makeUIViewController(context: UIViewControllerRepresentableContext<NavigationBarConfigurator>) -> UIViewController {
+public struct NavigationBarConfigurator: UIViewControllerRepresentable {
+    var configure: (UINavigationController) -> Void
+    public init(configure: @escaping (UINavigationController) -> Void) {
+        self.configure = configure
+    }
+    
+    public func makeUIViewController(context: UIViewControllerRepresentableContext<NavigationBarConfigurator>) -> UIViewController {
         UIViewController()
     }
-    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<NavigationBarConfigurator>) {
+    
+    public func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<NavigationBarConfigurator>) {
         if let nc = uiViewController.navigationController {
             self.configure(nc)
         }
