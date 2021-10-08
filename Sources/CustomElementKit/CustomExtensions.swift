2968ac53a3038885a85bcc517f60b3fdcc1a7343
Edward Wang <edwardwangxy@gmail.com>
2021-10-08 20:22:19 +0800

    add rounded support for uifont

--- Sources/CustomElementKit/CustomExtensions.swift
+++ Sources/CustomElementKit/CustomExtensions.swift
@@ -72,3 +72,17 @@ public extension UIImage {
         return UIGraphicsGetImageFromCurrentImageContext()
     }
 }
+
+public extension UIFont {
+    class func rounded(ofSize size: CGFloat, weight: UIFont.Weight) -> UIFont {
+        let systemFont = UIFont.systemFont(ofSize: size, weight: weight)
+        let font: UIFont
+        
+        if let descriptor = systemFont.fontDescriptor.withDesign(.rounded) {
+            font = UIFont(descriptor: descriptor, size: size)
+        } else {
+            font = systemFont
+        }
+        return font
+    }
+}
