e67cfb4ca0db4ff8325589632386f8db093ecaed
Edward Wang <admin@uwwxy.com>
2021-12-03 16:45:14 +0800

    add custom scroll view with offset

--- CustomElementKit.podspec
+++ CustomElementKit.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
     s.name         = "CustomElementKit"
-    s.version      = "1.1.41"
+    s.version      = "1.1.42"
     s.summary      = "SwiftUI Custom Elements"
     s.description  = <<-DESC
     FAT Theme Maker SwiftUI Custom Elements
---
+++ Sources/CustomElementKit/CustomScrollView.swift
@@ -0,0 +1,47 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Xiangyu Wang on 12/3/21.
+//
+
+import SwiftUI
+
+public struct CustomOffsetPreferenceKey: PreferenceKey {
+    public static var defaultValue: CGFloat = .zero
+    public static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {}
+}
+
+public struct CustomScrollView<Content: View>: View {
+    
+    let axes: Axis.Set
+    let showsIndicators: Bool
+    let content: Content
+    let offsetChange: (CGFloat) -> Void
+    let spaceName: String = UUID().uuidString
+    
+    public init(axes: Axis.Set = .vertical, showsIndicators: Bool = false, offsetChange: @escaping (CGFloat) -> Void, content: () -> Content) {
+        self.axes = axes
+        self.showsIndicators = showsIndicators
+        self.content = content()
+        self.offsetChange = offsetChange
+    }
+    
+    public var body: some View {
+        ScrollView(self.axes, showsIndicators: self.showsIndicators) {
+            GeometryReader { proxy in
+                Color.clear
+                    .preference(
+                        key: CustomOffsetPreferenceKey.self,
+                        value: self.axes == .vertical ? proxy.frame(in: .named(self.spaceName)).minY : proxy.frame(in: .named(self.spaceName)).minX
+                    )
+            }
+            .frame(height: 0)
+            self.content
+        }
+        .coordinateSpace(name: self.spaceName)
+        .onPreferenceChange(CustomOffsetPreferenceKey.self, perform: self.offsetChange)
+    }
+}
+
+
