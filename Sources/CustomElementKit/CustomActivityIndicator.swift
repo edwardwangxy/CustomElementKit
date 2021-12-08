aae9736f985d7ac570ab47491bb026164e7fc7d8
Edward Wang <admin@uwwxy.com>
2021-12-08 22:01:54 +0800

    update activity indicator to allow color change

--- Sources/CustomElementKit/CustomActivityIndicator.swift
+++ Sources/CustomElementKit/CustomActivityIndicator.swift
@@ -13,14 +13,20 @@ public struct CustomActivityIndicator: UIViewRepresentable {
 
     @Binding public var isAnimating: Bool
     public let style: UIActivityIndicatorView.Style
+    let color: UIColor?
     
-    public init(isAnimating: Binding<Bool>, style: UIActivityIndicatorView.Style) {
+    public init(isAnimating: Binding<Bool> = .constant(true), style: UIActivityIndicatorView.Style, color: UIColor? = nil) {
         self._isAnimating = isAnimating
         self.style = style
+        self.color = color
     }
 
     public func makeUIView(context: UIViewRepresentableContext<CustomActivityIndicator>) -> UIActivityIndicatorView {
-        return UIActivityIndicatorView(style: style)
+        let indicator = UIActivityIndicatorView(style: self.style)
+        if let getColor = self.color {
+            indicator.color = getColor
+        }
+        return indicator
     }
 
     public func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<CustomActivityIndicator>) {
