aa0c9361fa1e6fae1d2eb49a5acb8003258071cb
Edward Wang <admin@uwwxy.com>
2021-12-13 21:19:03 +0800

    add customasyncimage

--- CustomElementKit.podspec
+++ CustomElementKit.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
     s.name         = "CustomElementKit"
-    s.version      = "1.1.47"
+    s.version      = "1.1.48"
     s.summary      = "SwiftUI Custom Elements"
     s.description  = <<-DESC
     FAT Theme Maker SwiftUI Custom Elements
--- Sources/CustomElementKit/CustomAsyncImage.swift
+++ Sources/CustomElementKit/CustomAsyncImage.swift
@@ -1,34 +1,91 @@
 //
-//  SwiftUIView.swift
-//  
+//  CustomAsyncImage.swift
+//  FATThemeProjectManager
 //
-//  Created by Xiangyu Wang on 11/11/21.
+//  Created by Xiangyu Wang on 12/13/21.
 //
 
 import SwiftUI
+import Combine
 
 public struct CustomAsyncImage: View {
     
-    let imageURL: URL?
-    @State var fetchedImage: UIImage? = nil
-    
-    public init(url: URL?) {
-        self.imageURL = url
+    public enum CachePolicy {
+        case reload
+        case cached
     }
     
-    public var body: some View {
-        Image(uiImage: self.fetchedImage ?? UIImage())
-            .onAppear {
-                if self.fetchedImage == nil {
-                    DispatchQueue.global(qos: .utility).async {
-                        if let getURL = self.imageURL, let data = try? Data(contentsOf: getURL), let image = UIImage(data: data) {
-                            DispatchQueue.main.async {
-                                self.fetchedImage = image
-                            }
-                        }
-                    }
+    let url: URL
+    @State var image: UIImage? = nil
+    @State private var loader: AnyCancellable? = nil
+    private let activeResizable: Bool
+    private let cachePolicy: CachePolicy
+    @State
+    private var placeholder: AnyView?
+    
+    public init(url: URL, cachePolicy: CachePolicy = .cached, resizable: Bool = true) {
+        self.url = url
+        self.cachePolicy = cachePolicy
+        self.activeResizable = resizable
+    }
+    
+    enum ImageLoadError: Error {
+        case loadFail
+    }
+    
+    func loadImage() -> AnyPublisher<UIImage, Error> {
+        var request = URLRequest(url: self.url)
+        switch self.cachePolicy {
+        case .reload:
+            request.cachePolicy = .reloadIgnoringLocalCacheData
+        case .cached:
+            request.cachePolicy = .returnCacheDataElseLoad
+        }
+        request.cachePolicy = .reloadIgnoringLocalCacheData
+        return URLSession(configuration: .default).dataTaskPublisher(for: request)
+            .tryMap { (data, response) in
+                guard let image = UIImage(data: data) else {
+                    throw ImageLoadError.loadFail
                 }
+                return image
             }
+            .eraseToAnyPublisher()
+    }
+    
+    public func placeholder<Content: View>(@ViewBuilder _ content: () -> Content) -> Self {
+        self.placeholder = AnyView(content())
+        return self
+    }
+
+    public var body: some View {
+        ZStack {
+            if let getImage = self.image {
+                if self.activeResizable {
+                    Image(uiImage: getImage)
+                        .resizable()
+                } else {
+                    Image(uiImage: getImage)
+                }
+            } else if let placeholder = placeholder {
+                placeholder
+            }
+        }
+        .scaledToFill()
+        .onAppear {
+            self.loader = self.loadImage()
+                .subscribe(on: DispatchQueue.global(qos: .utility), options: nil)
+                .receive(on: DispatchQueue.main, options: nil)
+                .sink(receiveCompletion: { _ in
+                    
+                }, receiveValue: { image in
+                    self.image = image
+                })
+        }
+        .onDisappear {
+            self.loader?.cancel()
+            self.image = nil
+        }
     }
 }
+
 
