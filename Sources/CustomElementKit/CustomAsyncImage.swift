7fc1a02a8c8680d89e34b3c1ca67dc11e099d46b
Edward Wang <admin@uwwxy.com>
2021-12-14 01:53:34 +0800

    fix remote async image

--- Sources/CustomElementKit/CustomAsyncImage.swift
+++ Sources/CustomElementKit/CustomAsyncImage.swift
@@ -22,10 +22,17 @@ public struct CustomAsyncImage: View {
     private let cachePolicy: CachePolicy
     @State
     private var placeholder: AnyView?
+    private var id: String?
+    private var fm = FileManager.default
     
-    public init(url: URL, cachePolicy: CachePolicy = .cached, resizable: Bool = true) {
+    public init(url: URL, customCacheID: String? = nil, cachePolicy: CachePolicy = .cached, resizable: Bool = true) {
         self.url = url
-        self.cachePolicy = cachePolicy
+        self.id = customCacheID
+        if self.url.isFileURL {
+            self.cachePolicy = .reload
+        } else {
+            self.cachePolicy = cachePolicy
+        }
         self.activeResizable = resizable
     }
     
@@ -34,21 +41,71 @@ public struct CustomAsyncImage: View {
     }
     
     func loadImage() -> AnyPublisher<UIImage, Error> {
-        var request = URLRequest(url: self.url)
+        
         switch self.cachePolicy {
         case .reload:
+            var request = URLRequest(url: self.url)
             request.cachePolicy = .reloadIgnoringLocalCacheData
-        case .cached:
-            request.cachePolicy = .returnCacheDataElseLoad
-        }
-        return URLSession(configuration: .default).dataTaskPublisher(for: request)
-            .tryMap { (data, response) in
-                guard let image = UIImage(data: data) else {
-                    throw ImageLoadError.loadFail
+            return URLSession(configuration: .default).dataTaskPublisher(for: request)
+                .tryMap { (data, response) in
+                    guard let image = UIImage(data: data) else {
+                        throw ImageLoadError.loadFail
+                    }
+                    return image
                 }
-                return image
+                .eraseToAnyPublisher()
+        case .cached:
+            var lastPath = self.url.lastPathComponent
+            if let getCustomID = self.id {
+                lastPath = getCustomID
             }
-            .eraseToAnyPublisher()
+            if let cachedURL = try? self.fm.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: true), self.fm.fileExists(atPath: cachedURL.appendingPathComponent(lastPath).path) {
+                var request = URLRequest(url: cachedURL.appendingPathComponent(lastPath))
+                request.cachePolicy = .returnCacheDataElseLoad
+                return URLSession(configuration: .default).dataTaskPublisher(for: request)
+                    .tryMap { (data, response) in
+                        guard let image = UIImage(data: data) else {
+                            throw ImageLoadError.loadFail
+                        }
+                        return image
+                    }
+                    .eraseToAnyPublisher()
+            } else {
+                return Deferred {
+                    Future<UIImage, Error>.init { promise in
+                        var request = URLRequest(url: self.url)
+                        request.cachePolicy = .returnCacheDataElseLoad
+                        URLSession.shared.downloadTask(with: url) { localURL, _, error in
+                            if let getErr = error {
+                                promise(.failure(getErr))
+                            } else if let getURL = localURL {
+                                DispatchQueue.global(qos: .background).async {
+                                    if let cachedURL = try? self.fm.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: true) {
+                                        try? self.fm.removeItem(at: cachedURL.appendingPathComponent(lastPath))
+                                        try? self.fm.copyItem(at: getURL, to: cachedURL.appendingPathComponent(lastPath))
+                                    }
+                                }
+                                do {
+                                    let data = try Data(contentsOf: getURL)
+                                    if let getImage = UIImage(data: data) {
+                                        promise(.success(getImage))
+                                    } else {
+                                        promise(.failure(ImageLoadError.loadFail))
+                                    }
+                                } catch {
+                                    promise(.failure(error))
+                                }
+                            } else {
+                                promise(.failure(ImageLoadError.loadFail))
+                            }
+                        }
+                        .resume()
+                    }
+                }
+                .eraseToAnyPublisher()
+            }
+        }
+        
     }
     
     public func placeholder<Content: View>(@ViewBuilder _ content: () -> Content) -> Self {
@@ -85,5 +142,4 @@ public struct CustomAsyncImage: View {
         }
     }
 }
-
 
