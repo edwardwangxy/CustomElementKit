beac38c0af633efaee78937e0fc05d8c1e41f185
Edward Wang <admin@uwwxy.com>
2022-01-02 11:19:59 +0800

    add background cover

--- CustomElementKit.podspec
+++ CustomElementKit.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
     s.name         = "CustomElementKit"
-    s.version      = "1.1.64"
+    s.version      = "1.1.65"
     s.summary      = "SwiftUI Custom Elements"
     s.description  = <<-DESC
     FAT Theme Maker SwiftUI Custom Elements
--- Sources/CustomElementKit/CustomSheet.swift
+++ Sources/CustomElementKit/CustomSheet.swift
@@ -9,10 +9,10 @@ import SwiftUI
 import Combine
 
 public extension View {
-    func customSheet<Content: View>(isPresented: Binding<Bool>, animated: Bool = true, canDragDismiss: Bool = true, style: UIModalPresentationStyle = .automatic, transition: UIModalTransitionStyle = .coverVertical, attemptDismiss: (() -> Void)? = nil, presentComplete: (() -> Void)? = nil, dismissComplete: (() -> Void)? = nil, @ViewBuilder content: @escaping () -> Content) -> some View {
+    func customSheet<Content: View>(isPresented: Binding<Bool>, withBackground: Bool = true, animated: Bool = true, canDragDismiss: Bool = true, style: UIModalPresentationStyle = .automatic, transition: UIModalTransitionStyle = .coverVertical, attemptDismiss: (() -> Void)? = nil, presentComplete: (() -> Void)? = nil, dismissComplete: (() -> Void)? = nil, @ViewBuilder content: @escaping () -> Content) -> some View {
         return self
             .background(
-                CustomSheet(content: content, animated: animated, canDragDismiss: canDragDismiss, style: style, transition: transition, attemptDismiss: attemptDismiss, presentComplete: presentComplete, dismissComplete: dismissComplete, isPresented: isPresented)
+                CustomSheet(content: content, withBackgroundCover: withBackground, animated: animated, canDragDismiss: canDragDismiss, style: style, transition: transition, attemptDismiss: attemptDismiss, presentComplete: presentComplete, dismissComplete: dismissComplete, isPresented: isPresented)
             )
     }
 }
@@ -21,7 +21,10 @@ struct CustomSheet<Content: View>: UIViewControllerRepresentable {
     func makeCoordinator() -> Coordinator {
         return Coordinator(parent: self)
     }
-    let sheet: UINavigationController
+    let content: () -> Content
+    let style: UIModalPresentationStyle
+    let transition: UIModalTransitionStyle
+    let withBackgroundCover: Bool
     var animated: Bool
     var canDragDismiss: Bool
     var attemptDismiss: (() -> Void)?
@@ -29,13 +32,11 @@ struct CustomSheet<Content: View>: UIViewControllerRepresentable {
     var dismissComplete: (() -> Void)?
     @Binding var isPresented: Bool
     
-    init(content: @escaping () -> Content, animated: Bool, canDragDismiss: Bool, style: UIModalPresentationStyle, transition: UIModalTransitionStyle, attemptDismiss: (() -> Void)?, presentComplete: (() -> Void)?, dismissComplete: (() -> Void)?, isPresented: Binding<Bool>) {
-        let hc = UIHostingController(rootView: content())
-        hc.view.backgroundColor = .clear
-        self.sheet = UINavigationController(rootViewController: hc)
-        self.sheet.modalPresentationStyle = style
-        self.sheet.modalTransitionStyle = transition
-        self.sheet.view.backgroundColor = .clear
+    init(content: @escaping () -> Content, withBackgroundCover: Bool, animated: Bool, canDragDismiss: Bool, style: UIModalPresentationStyle, transition: UIModalTransitionStyle, attemptDismiss: (() -> Void)?, presentComplete: (() -> Void)?, dismissComplete: (() -> Void)?, isPresented: Binding<Bool>) {
+        self.withBackgroundCover = withBackgroundCover
+        self.content = content
+        self.style = style
+        self.transition = transition
         self.animated = animated
         self.canDragDismiss = canDragDismiss
         self.presentComplete = presentComplete
@@ -45,24 +46,47 @@ struct CustomSheet<Content: View>: UIViewControllerRepresentable {
     }
     
     func makeUIViewController(context: Context) -> UIViewController {
-        
         return UIViewController()
     }
     
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
         if self.isPresented {
-            uiViewController.present(self.sheet, animated: self.animated, completion: self.presentComplete)
-            self.sheet.presentationController?.delegate = context.coordinator
+            let hc = UIHostingController(rootView: self.content())
+            hc.view.backgroundColor = .clear
+            let sheet = UINavigationController(rootViewController: hc)
+            sheet.modalPresentationStyle = self.style
+            sheet.modalTransitionStyle = self.transition
+            sheet.view.backgroundColor = .clear
+            if self.withBackgroundCover {
+                uiViewController.present(sheet, animated: self.animated, completion: {
+                    UIView.animate(withDuration: 0.15) {
+                        sheet.view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3)
+                    }
+                    self.presentComplete?()
+                })
+                context.coordinator.presetedSheet = sheet
+            } else {
+                uiViewController.present(sheet, animated: self.animated, completion: self.presentComplete)
+            }
+            sheet.presentationController?.delegate = context.coordinator
+            
         } else {
-            uiViewController.presentedViewController?.dismiss(animated: self.animated, completion: {
-                self.isPresented = false
-                self.dismissComplete?()
-            })
+            if let getSheet = context.coordinator.presetedSheet {
+                UIView.animate(withDuration: 0.15) {
+                    getSheet.view.backgroundColor = .clear
+                } completion: { _ in
+                    uiViewController.presentedViewController?.dismiss(animated: self.animated, completion: self.dismissComplete)
+                }
+            } else {
+                uiViewController.presentedViewController?.dismiss(animated: self.animated, completion: self.dismissComplete)
+            }
         }
     }
     
     class Coordinator: NSObject, UIAdaptivePresentationControllerDelegate {
         let parent: CustomSheet
+        var presetedSheet: UINavigationController?
+        
         init(parent: CustomSheet) {
             self.parent = parent
         }
