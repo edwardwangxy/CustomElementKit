fe24d22d0f7a7837c675d102e5668e54cc4a1377
Edward Wang <admin@uwwxy.com>
2022-01-02 10:42:14 +0800

    fix action sheet

--- Sources/CustomElementKit/CustomActionSheet.swift
+++ Sources/CustomElementKit/CustomActionSheet.swift
@@ -28,7 +28,9 @@ public struct CustomActionSheet: UIViewControllerRepresentable {
     init(title: String?, message: String?, actionButtons: [ActionButton], presentComplete: (() -> Void)?, dismissComplete: (() -> Void)?, isPresented: Binding<Bool>) {
         self.alertController = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
         for eachBtn in actionButtons {
-            self.alertController.addAction(eachBtn.button)
+            self.alertController.addAction(eachBtn.generateButton {
+                isPresented.wrappedValue = false
+            })
         }
         self.presentComplete = presentComplete
         self.dismissComplete = dismissComplete
@@ -46,8 +48,11 @@ public struct CustomActionSheet: UIViewControllerRepresentable {
     
     public func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
         if self.isPresented {
-            uiViewController.present(self.alertController, animated: true, completion: self.presentComplete)
-            self.alertController.presentationController?.delegate = context.coordinator
+            uiViewController.present(self.alertController, animated: true, completion: {
+                let tapGesture = UITapGestureRecognizer(target: context.coordinator, action: #selector(context.coordinator.dismissAlertController))
+                self.alertController.view.superview?.subviews[0].addGestureRecognizer(tapGesture)
+                self.presentComplete?()
+            })
         } else {
             uiViewController.presentedViewController?.dismiss(animated: true, completion: {
                 self.isPresented = false
@@ -57,25 +62,36 @@ public struct CustomActionSheet: UIViewControllerRepresentable {
     }
     
     public struct ActionButton {
-        let button: UIAlertAction
+        let title: String
+        let image: UIImage?
+        let style: UIAlertAction.Style
+        let handler: (() -> Void)?
         
-        init(title: String, image: UIImage? = nil, style: UIAlertAction.Style, handler: ((UIAlertAction) -> Void)?) {
-            let btn = UIAlertAction(title: title, style: style, handler: handler)
-            if let getImage = image {
-                btn.setValue(getImage, forKey: "image")
-            }
-            self.button = btn
+        init(title: String, image: UIImage? = nil, style: UIAlertAction.Style, handler: (() -> Void)?) {
+            self.title = title
+            self.image = image
+            self.style = style
+            self.handler = handler
         }
         
-        public static func `default`(title: String, image: UIImage? = nil, handler: ((UIAlertAction) -> Void)? = nil) -> ActionButton {
+        func generateButton(preAction: @escaping () -> Void) -> UIAlertAction {
+            let btn = UIAlertAction(title: self.title, style: self.style) { _ in
+                preAction()
+                self.handler?()
+            }
+            btn.setValue(self.image, forKey: "image")
+            return btn
+        }
+        
+        public static func `default`(title: String, image: UIImage? = nil, handler: (() -> Void)? = nil) -> ActionButton {
             return ActionButton(title: title, image: image, style: .default, handler: handler)
         }
         
-        public static func destructive(title: String, image: UIImage? = nil, handler: ((UIAlertAction) -> Void)? = nil) -> ActionButton {
+        public static func destructive(title: String, image: UIImage? = nil, handler: (() -> Void)? = nil) -> ActionButton {
             return ActionButton(title: title, image: image, style: .destructive, handler: handler)
         }
         
-        public static func cancel(title: String, image: UIImage? = nil, handler: ((UIAlertAction) -> Void)? = nil) -> ActionButton {
+        public static func cancel(title: String, image: UIImage? = nil, handler: (() -> Void)? = nil) -> ActionButton {
             return ActionButton(title: title, image: image, style: .cancel, handler: handler)
         }
     }
@@ -87,6 +103,10 @@ public struct CustomActionSheet: UIViewControllerRepresentable {
         }
         
         public func presentationControllerDidDismiss(_ presentationController: UIPresentationController) {
+            self.parent.isPresented = false
+        }
+        
+        @objc func dismissAlertController(){
             self.parent.isPresented = false
         }
         
