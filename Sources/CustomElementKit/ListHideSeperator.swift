9b96e1a8646bdfce24677dc29869ae9d293e5801
Edward Wang <edwardwangxy@gmail.com>
2022-03-29 13:45:32 +0800

    add list modifier

---
+++ Sources/CustomElementKit/ListHideSeperator.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  
+//
+//  Created by Xiangyu Wang on 3/29/22.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+public struct ListHideSeperator: ViewModifier {
+    let bgColor: Color
+    
+    public init(bgColor: Color = Color(red: 0, green: 0, blue: 0)) {
+        self.bgColor = bgColor
+    }
+    
+    public func body(content: Content) -> some View {
+        if #available(iOS 15, *) {
+            content
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                .listRowInsets(.init(top: -1, leading: -1, bottom: -1, trailing: -1))
+                .listRowSeparator(.hidden)
+        } else {
+            content
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                .listRowInsets(.init(top: -1, leading: -1, bottom: -1, trailing: -1))
+                .background(self.bgColor)
+        }
+    }
+}
+
+public extension View {
+    func listItemRemoveStyle(bgColor: Color = Color(red: 0, green: 0, blue: 0)) -> some View {
+        return self.modifier(ListHideSeperator(bgColor: bgColor))
+    }
+}
